31c54cea6f225dd8806447dbd24ef8697a267c43
rusakova92 <149253830+rusakova92@users.noreply.github.com>
2024-04-30 00:38:38 +1000

    Update sast.py

--- sast.py
+++ sast.py
@@ -3,17 +3,17 @@ import subprocess
 from docx import Document
 
 # Клонируем репозиторий
-repo_url = 'https://github.com/rusakova92.git'
-repo_dir = 'repository_folder'
-repo = git.Repo.clone_from(repo_url, repo_dir)
+repository_url = 'https://github.com/rusakova92.it'
+repository_dir = 'repository_folder'
+repo = git.Repo.clone_from(repository_url, repository_dir)
 
 # Запуск SAST-анализатора
-output = subprocess.check_output(['SAST_analyzer', repo_dir])
+output = subprocess.check_output(['SAST_analyzer', repository_dir])
 
 # Создание документ с результатами анализа
 doc = Document()
 doc.add_heading('Результаты анализа уязвимостей', level=1)
-doc.add_paragraph('Репозиторий: {}'.format(repo_url))
+doc.add_paragraph('Репозиторий: {}'.format(repository_url))
 doc.add_paragraph('Результаты анализа:')
 doc.add_paragraph(output.decode('utf-8'))
 
